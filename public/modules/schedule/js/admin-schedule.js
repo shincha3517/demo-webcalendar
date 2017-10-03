4af13435df664576aba00fb733daaf804ce67245
Eric Dao <vu.dao@apps-cyclone.com>
2017-10-03 12:56:49 +0700

    css

--- public/modules/schedule/js/admin-schedule.js
+++ public/modules/schedule/js/admin-schedule.js
@@ -304,11 +304,11 @@ var Home = {
                     if(data.status == 1){
                         $('#ddUser').select2({
                             data: data.result,
-                            sorter: function(data) {
-                                return data.sort(function(a, b) {
-                                    return a.text < b.text ? -1 : a.text > b.text ? 1 : 0;
-                                });
-                            }
+                            // sorter: function(data) {
+                            //     return data.sort(function(a, b) {
+                            //         return a.text < b.text ? -1 : a.text > b.text ? 1 : 0;
+                            //     });
+                            // }
                             // allowClear: true
                         });
                     }
@@ -375,6 +375,7 @@ var Home = {
                     console.log( errorThrown );
                 }
             });
+            return;
         });
 
 
