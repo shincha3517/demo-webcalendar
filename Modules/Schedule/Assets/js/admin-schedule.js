42efaf66a9c99004b2178737a35b34282d287395
vudao <vu.dao@apps0cyclone.com>
2018-01-01 20:54:47 +0700

    show log date

--- Modules/Schedule/Assets/js/admin-schedule.js
+++ Modules/Schedule/Assets/js/admin-schedule.js
@@ -695,7 +695,7 @@ var Home = {
                 var formattedDate = curr_date + "/" + curr_month + "/" + curr_year;
                 var curr_week = moment(d, "MM-DD-YYYY").week();
 
-                // console.log(formattedDate);
+                console.log(formattedDate);
 
                 var holidaysWeekNumber = [11,22,23,24,25,36,47,48,49,50,51,52];
                 if(holidaysWeekNumber.indexOf(curr_week) >= 0 ){
